eaf9a07b51099091d597d47b417b1c504c6c448b
akshaydig <you@example.com>
2021-12-13 10:03:23 +0530

    Added: Program to find roots of Quodratic Equation

---
+++ src/com/bl/basicprograms/Quadratic.java
@@ -0,0 +1,17 @@
+package com.bl.basicprograms;
+
+import java.util.Scanner;
+
+public class Quadratic {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a,b,c Values of Equation ax*x+b*x+c");
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		double delta = (b * b) - (4 * a * c);
+		double rootOne = (double) (-b + Math.sqrt(delta)) / (2 * a);
+		double rootTwo = (double) (-b - Math.sqrt(delta)) / (2 * a);
+		System.out.println("The two roots of Equation are :" + rootOne + " and " + rootTwo);
+	}
+}
