a0fa338333ef0dc101d86ad5cabb61d7038e7bf8
akshaydig <you@example.com>
2021-12-13 09:55:58 +0530

    Added:  Program to find Triplets those adds to zero

---
+++ src/com/bl/basicprograms/TripletsAddsToZero.java
@@ -0,0 +1,34 @@
+package com.bl.basicprograms;
+
+import java.util.Scanner;
+
+public class TripletsAddsToZero {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter length of an array :");
+		int arrayLength = scanner.nextInt();
+		int mainArray[] = new int[arrayLength];
+		int c;
+		for (c = 0; c < arrayLength; c++) {
+			System.out.println("Enter element of array :");
+			mainArray[c] = scanner.nextInt();
+		}
+		findTriplets(mainArray, arrayLength);
+	}
+
+	static void findTriplets(int[] array, int n) {
+		boolean found = false;
+		for (int i = 0; i < n - 2; i++) {
+			for (int j = i + 1; j < n - 1; j++) {
+				for (int k = j + 1; k < n; k++) {
+					if (array[i] + array[j] + array[k] == 0) {
+						found = true;
+						System.out.println("The Triplets are :" + array[i] + " " + array[j] + " " + array[k]);
+					}
+				}
+			}
+		}
+		if (found == false)
+			System.out.println(" not exist ");
+	}
+}
