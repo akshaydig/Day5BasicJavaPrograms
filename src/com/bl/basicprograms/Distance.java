28cf7f9a230268adf3ff3756a064f64f4a717fa9
akshaydig <you@example.com>
2021-12-13 09:59:03 +0530

    Added: Functionality to find Euclidean Distance

---
+++ src/com/bl/basicprograms/Distance.java
@@ -0,0 +1,15 @@
+package com.bl.basicprograms;
+
+import java.util.Scanner;
+
+public class Distance {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter x Component");
+		int x = scanner.nextInt();
+		System.out.println("Enter y Component");
+		int y = scanner.nextInt();
+		double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+		System.out.println("The Euclidean distance is :" + distance);
+	}
+}
