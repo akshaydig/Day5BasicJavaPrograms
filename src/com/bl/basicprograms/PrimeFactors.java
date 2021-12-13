29b7896c83d63d9bd2ac7bd0f18ab7d106841693
akshaydig <you@example.com>
2021-12-13 09:28:34 +0530

    Added: Program to find PrimeFactors of Number

---
+++ src/com/bl/basicprograms/PrimeFactors.java
@@ -0,0 +1,25 @@
+package com.bl.basicprograms;
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a Number");
+
+		int number = scanner.nextInt();
+		int isPrime = 0;
+		int countOne, countTwo;
+		for (countOne = 2; (countOne) <= number; countOne++) {
+			if (number % countOne == 0) {
+				isPrime = 1;
+				for (countTwo = 2; countTwo <= (countOne / 2); countTwo++) {
+					if ((countOne % countTwo) == 0)
+						isPrime = 0;
+				}
+				if (isPrime == 1)
+					System.out.println("Prime factor is :" + countOne);
+			}
+		}
+	}
+}
