dc25b54f2f64895c499a44bd2acb0ca898cd9fba
akshaydig <you@example.com>
2021-12-13 09:43:01 +0530

    Added: Functionality to find an alphabet is vowel or Consonant

---
+++ src/com/bl/basicprograms/VowelConsonant.java
@@ -0,0 +1,15 @@
+package com.bl.basicprograms;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter an Alphabet");
+		char alphabet = scanner.next().charAt(0);
+		if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u')
+			System.out.println(alphabet + " is vowel");
+		else
+			System.out.println(alphabet + " is consonant");
+	}
+}
