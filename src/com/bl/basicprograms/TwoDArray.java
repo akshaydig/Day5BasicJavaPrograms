59e75e092d09837923cafbcd391fab1a8bb86e77
akshaydig <you@example.com>
2021-12-13 09:51:36 +0530

    Added: Functionality to print 2DArray

---
+++ src/com/bl/basicprograms/TwoDArray.java
@@ -0,0 +1,27 @@
+package com.bl.basicprograms;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter number of Rows and Columns");
+		int row = scanner.nextInt();
+		int column = scanner.nextInt();
+		int array[][] = new int[row][column];
+		int i, j;
+		for (i = 0; i < row; i++) {
+			for (j = 0; j < column; j++) {
+				System.out.println("Enter element of array");
+				int element = scanner.nextInt();
+				array[i][j] = element;
+			}
+		}
+		for (i = 0; i < row; i++) {
+			System.out.println();
+			for (j = 0; j < column; j++) {
+				System.out.print(" " + array[i][j]);
+			}
+		}
+	}
+}
